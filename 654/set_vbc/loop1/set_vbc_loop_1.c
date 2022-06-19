b70d7b017ff465768621431479a7519befe0b174
yyZhang1024 <linglingzero000@gmail.com>
2022-06-19 11:31:43 +0800

    654 set_vbc loop1
     ! __loop__ 1370
    need: -fno-math-errno

---
+++ 654/set_vbc/loop1/set_vbc_loop_1.c
@@ -0,0 +1,26 @@
+#include <math.h>
+#include "rdrg2.h"
+#include "u.h"
+#include "v.h"
+
+// ! __loop__ 1370
+// !!!!!!!!!!!!!!!! need: -fno-math-errno
+void set_vbc_loop1(double *restrict v, double *restrict bustr, double *restrict u, double  rdrg2)
+{
+   double cff1, cff2; 
+   for(int i = 1; i < 128 * 15; i++)
+   {
+        cff1 = 0.25 *(   v[i] + (v + 128)[i] + v[i-1] + (v + 128)[i-1]   ) ;
+        cff2 = sqrt(u[i] * u[i]  + cff1 * cff1);
+        bustr[i] = rdrg2 * u[i] * cff2;
+   }
+}
+double bustr[2048];
+void input_data_call()
+{
+    set_vbc_loop1(v, bustr, u, rdrg2);
+}
+int main()
+{
+    input_data_call();
+}
