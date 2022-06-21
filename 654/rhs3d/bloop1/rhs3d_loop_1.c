363865a1e34373e96a03f119c4ba7d3737c510ea
yyZhang1024 <linglingzero000@gmail.com>
2022-06-21 18:34:34 +0800

    654 rhs3d loop 1,2
    big data

---
+++ 654/rhs3d/bloop1/rhs3d_loop_1.c
@@ -0,0 +1,33 @@
+#include "dmde.h"
+#include "dndx.h"
+#include "fomn.h"
+#include "Hz.h"
+#include "u_1.h"
+#include "u.h"
+#include "v_1.h"
+#include "v.h"
+// 1442
+void rhs3d_loop_1(int n, double *Hz, double * fomn, double * v, double * v_1, double * u, double * u_1, double * dndx, double * dmde, double * UFx, double * VFe)
+{
+   double cff;
+   #pragma clang loop vectorize(enable)
+   for(int i = 0; i < n; i++)
+   { 
+       cff = 0.5 * (v[i] + v_1[i]) * dndx[i] - (u[i] + u_1[i]) * dmde[i];
+       UFx[i] = cff * (v[i] + v_1[i]);
+       VFe[i] = cff * (u[i] + u_1[i]);
+   }
+}
+double UFx[129000];
+double VFe[129000];
+int n = 129000;
+void input_data_call()
+{
+    
+    rhs3d_loop_1(n, Hz, fomn, v, v_1, u, u_1, dndx, dmde, UFx, VFe);
+}
+
+int main()
+{
+    input_data_call();
+}
