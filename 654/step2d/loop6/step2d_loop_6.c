f340713a55fe180403c5faf83da1b47fcfc93951
yyZhang1024 <linglingzero000@gmail.com>
2022-06-21 13:58:57 +0800

    add step2d loop 1 6 7 8

---
+++ 654/step2d/loop6/step2d_loop_6.c
@@ -0,0 +1,59 @@
+#include "cff1.h"
+#include "DUon.h"
+#include "DVom.h"
+#include "fac.h"
+#include "gzeta.h"
+#include "h.h"
+#include "pm.h"
+#include "pn.h"
+#include "rhoS.h"
+#include "rhoA.h"
+#include "zeta.h"
+//   ! __loop_ 1560
+void step2d_loop_6(double*Dnew, double cff1, double fac, double * DUon, double * DVom, double * zeta, double * pm, double * pn, double * rhs_zeta, \
+                                            double * zeta_new, double * h, double * zwrk, double * gzeta, double * gzeta2, double * gzetaSA, double * rhoS, double * rhoA)
+{
+    /*
+            DO j=JstrV-1,Jend ! __loop_ 1560
+          DO i=IstrU-1,Iend
+            rhs_zeta(i,j)=(DUon(i,j)-DUon(i+1,j))+                      &
+     &                    (DVom(i,j)-DVom(i,j+1))
+            zeta_new(i,j)=zeta(i,j,kstp)+                               &
+     &                    pm(i,j)*pn(i,j)*cff1*rhs_zeta(i,j)
+            Dnew(i,j)=zeta_new(i,j)+h(i,j)
+!
+            zwrk(i,j)=0.5_r8*(zeta(i,j,kstp)+zeta_new(i,j))
+            gzeta(i,j)=(fac+rhoS(i,j))*zwrk(i,j)
+            gzeta2(i,j)=gzeta(i,j)*zwrk(i,j)
+            gzetaSA(i,j)=zwrk(i,j)*(rhoS(i,j)-rhoA(i,j))
+          END DO
+        END DO
+    */
+   #pragma clang loop vectorize(enable)
+    for(int i = 0; i < 128; i++)
+    {
+        rhs_zeta[i] = (DUon[i] - DUon[i+1]) + (DVom[i] - DVom[i+1]);
+        zeta_new[i] = zeta[i] + pm[i] * pn[i] * cff1 * rhs_zeta[i];
+        Dnew[i] = zeta_new[i] + h[i];
+        zwrk[i] = 0.5 * (zeta[i] + zeta_new[i]);
+        gzeta[i] = (fac + rhoS[i]) * zwrk[i];
+        gzeta2[i] = gzeta[i] * zwrk[i];
+        gzetaSA[i] = zwrk[i] * (rhoS[i] - rhoA[i]);
+
+    }
+}
+double zeta_new[2193];
+double Dnew[2193];
+double gzeta[2193];
+double zwrk[2193];
+double gzeta2[2193];
+double gzetaSA[2193];
+double rhs_zeta[2193];
+void input_data_call()
+{
+    step2d_loop_6(Dnew, cff1, fac, DUon, DVom, zeta, pm, pn, rhs_zeta, zeta_new, h, zwrk, gzeta, gzeta2, gzetaSA, rhoS, rhoA);
+}
+int main()
+{
+    input_data_call();
+}
