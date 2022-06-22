8e0fbb8c1e4d6b1e1ece67f4681e464ccc3b5974
yyZhang1024 <linglingzero000@gmail.com>
2022-06-22 10:37:14 +0800

    654 rho_eos loop2
    big data

---
+++ 654/brho_eos/loop2/rho_eos_loop_2.c
@@ -0,0 +1,25 @@
+#include "rho_eos_cff2.h"
+
+#include "rho_eos_rhoA.h"
+#include "rho_eos_rhoS_2.h"
+#include "rho_eos_z_w_0.h"
+#include "rho_eos_z_w_n.h"
+void rho_eos_loop_2(double *z_w_n ,  double* z_w_0, double cff2, double * rhoA, double * rhoS)
+{
+    double cff1;
+    for(int i = 0; i < 101376; i++)
+    {
+        cff1 = 1.0 / (z_w_n[i] - z_w_0[i]);
+        rhoA[i] = cff2 * cff1 * rhoA[i];
+        rhoS[i] = 2.0 * cff1 * cff1 * cff2 * rhoS[i];
+    }
+}
+
+void input_data_call()
+{
+    rho_eos_loop_2( z_w_n, z_w_0, cff2, rhoA, rhoS_2);
+}
+int main()
+{
+    input_data_call();
+}
